b0bc3776f603152b6032beb5af4cb4baba2f2b27
陳科旭 <syiu0415tw@gmail.com>
2018-03-05 12:52:31 +0800

    Update weather-observation-station-6.sql

--- weather-observation-station-6.sql
+++ weather-observation-station-6.sql
@@ -2,8 +2,8 @@
     Query CITY names starting with vowels from STATION
     Exclude duplicates from results
 */
-// IBM Db2, MySQL and Microsoft SQL Server
+-- IBM Db2, MySQL and Microsoft SQL Server
 SELECT DISTINCT CITY FROM STATION WHERE LEFT(CITY, 1) IN ('A', 'E', 'I', 'O', 'U');
 
-// Oracle SQL
+-- Oracle SQL
 SELECT DISTINCT CITY FROM STATION WHERE SUBSTR(CITY, 1, 1) IN ('A', 'E', 'I', 'O', 'U');
