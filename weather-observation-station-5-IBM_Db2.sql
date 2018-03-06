9e8848361926efdbdbdd77ec4b87b3fb76761b59
陳科旭 <syiu0415tw@gmail.com>
2018-03-06 12:27:46 +0800

    Delete weather-observation-station-5-IBM_Db2.sql

--- weather-observation-station-5-IBM_Db2.sql
+++
@@ -1,6 +0,0 @@
-/*
-    Query the shortest and longest CITY names from STATION
-    If there is more than one result, choose the one that comes first alphabetically
-*/
-SELECT CITY, LENGTH(CITY) FROM STATION WHERE LENGTH(CITY) = (SELECT MIN(LENGTH(CITY)) FROM STATION) ORDER BY CITY LIMIT 1;
-SELECT CITY, LENGTH(CITY) FROM STATION WHERE LENGTH(CITY) = (SELECT MAX(LENGTH(CITY)) FROM STATION) ORDER BY CITY LIMIT 1;
