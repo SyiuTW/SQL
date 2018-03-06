8b9110cd2ea202577cb848d4f15ab04612204be6
陳科旭 <syiu0415tw@gmail.com>
2018-03-06 12:28:00 +0800

    Delete weather-observation-station-5-Microsoft_SQL_Server.sql

--- weather-observation-station-5-Microsoft_SQL_Server.sql
+++
@@ -1,6 +0,0 @@
-/*
-    Query the shortest and longest CITY names from STATION
-    If there is more than one result, choose the one that comes first alphabetically
-*/
-SELECT TOP 1 CITY, LEN(CITY) FROM STATION WHERE LEN(CITY) = (SELECT MIN(LEN(CITY)) FROM STATION) ORDER BY CITY;
-SELECT TOP 1 CITY, LEN(CITY) FROM STATION WHERE LEN(CITY) = (SELECT MAX(LEN(CITY)) FROM STATION) ORDER BY CITY;
